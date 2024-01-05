af34a25de24eccb58b3b7edf45bf4f993447dead
BradenE19 <6115@holbertonstudents.com>
2024-01-05 14:06:57 -0600

    task1

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Import wait_random from the previous python file that you’ve written and write an async routine called wait_n that takes in 2 int arguments (in this order): n and max_delay. You will spawn wait_random n times with the specified max_delay."""
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """wait_n should return the list of all the delays (float values). The list of the delays should be in ascending order without using sort() because of concurrency."""
+    # create an empty list
+    my_list = []
+
+    # run concurrent tasks
+    for i in range(n):
+        # wait for each task and append results
+        my_list.append(wait_random(max_delay))
+    # sort list
+    sor = await asyncio.gather(*my_list)
+    return sorted(sor)
