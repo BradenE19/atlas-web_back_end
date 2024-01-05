816e31e4de43ac8affb5cfd61f8003f74667452c
BradenE19 <6115@holbertonstudents.com>
2024-01-05 14:22:21 -0600

    task4 fix?

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,16 +1,36 @@
 #!/usr/bin/env python3
-"""Take the code from wait_n and alter it into a new function task_wait_n. The code is nearly identical to wait_n except task_wait_random is being called."""
+"""
+Take the code from wait_n
+and alter it into a new function task_wait_n.
+The code is nearly identical to wait_n except
+task_wait_random is being called.
+"""
 import asyncio
 from typing import List
-task_wait_random = __import__('3-tasks').task_wait_random
+
+wait_n = __import__("1-concurrent_coroutines").wait_n
+task_wait_random = __import__("3-tasks").task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """return the list of all the delays (float values). The list of the
-    delays should be in ascending order without using sort() because
-    of concurrency."""
-    my_list = []
-    for i in range(n):
-        my_list.append(task_wait_random(max_delay))
-    sor = await asyncio.gather(*my_list)
-    return sorted(sor)
+    """
+    Function task_wait_n that takes in 2 int arguments
+
+    Args:
+        n (int): number of times to spawn task_wait_random
+        max_delay (int): max delay to wait_random
+
+    Returns:
+        List[float]: list of all delays in ascending order may be float values
+    """
+    if n <= 0:
+        return []
+
+    delay_list = []
+
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+
+    for task in asyncio.as_completed(tasks):
+        delay_list.append(await task)
+
+    return delay_list
