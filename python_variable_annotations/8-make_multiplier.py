182073004791a653e8370db4ab591b929fc81fd3
BradenE19 <6115@holbertonstudents.com>
2024-01-05 14:46:24 -0600

    fix 8?

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python3
-"""Write a type-annotated function make_multiplier that takes a float multiplier as argument"""
+"""
+Write a type-annotated function make_multiplier
+that takes a float multiplier as argument
+and returns a function that multiplies a float by multiplier.
+"""
 from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    """returns a function that multiplies a float by multiplier."""
-    def multiplier(n: float) -> float:
-        """returns a function that multiplies a float by multiplier."""
-        return n * multiplier
-    return multiplier
+    """
+    Function that multiplies a float by multiplier.
+    """
+    def multiplier_function(number: float) -> float:
+        """
+        Function that multiplies the function argument by
+        multiplier argument from the parent function.
+        """
+        return number * multiplier
+    return multiplier_function
