20995a672bff864fae53f6ce518c8fb53ad91005
BradenE19 <6115@holbertonstudents.com>
2024-01-18 12:46:03 -0600

    task 10

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -81,3 +81,33 @@ class BasicAuth(Auth):
         credentials = decoded_base64_authorization_header.split(':', 1)
 
         return (credentials[0], credentials[1])
+
+    def user_object_from_credentials(self,
+                                     user_email: str, user_pwd: str
+                                     ) -> TypeVar('User'):
+        """
+            Make or Get User from credentials
+
+            Args:
+                user_email: Email from user
+                user_pwd: Currrent user
+
+            Return:
+                User instance or None
+        """
+        if user_email is None or type(user_email) != str or\
+           user_pwd is None or type(user_pwd) != str:\
+
+            return None
+
+        try:
+            exist_user: List[TypeVar('User')]
+            exist_user = User.search({"email": user_email})
+        except Exception:
+            return None
+
+        for user in exist_user:
+            if user.is_valid_password(user_pwd):
+                return user
+
+        return None
