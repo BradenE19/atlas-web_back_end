db3720bd45bca3e257c2620ca56e2f018910c1b6
BradenE19 <6115@holbertonstudents.com>
2024-01-18 12:32:09 -0600

    pycode fix for task 8

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -58,7 +58,7 @@ class BasicAuth(Auth):
             return None
 
         return data_decode.decode('utf-8')
-    
+
     def extract_user_credentials(self,
                                  decoded_base64_authorization_header: str
                                  ) -> (str, str):
