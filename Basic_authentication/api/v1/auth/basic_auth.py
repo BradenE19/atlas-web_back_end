49102237747b37747c5dd2f0ee0c3b53c5132acd
BradenE19 <6115@holbertonstudents.com>
2024-01-18 12:15:07 -0600

    task7 attempt

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -11,3 +11,25 @@ class BasicAuth(Auth):
     """ Basic Auth class """
     def __init__(self):
         """Constructor"""
+
+        def extract_base64_authorization_header(
+                                            self,
+                                            authorization_header: str
+                                            ) -> str:
+        """
+            Extract header in base64
+
+            Args:
+                authorization_header: string in base64
+
+            Return:
+                Header in base64 or None
+        """
+        if authorization_header is None\
+           or type(authorization_header) != str\
+           or not authorization_header.startswith('Basic ')\
+           and not authorization_header.endswith(' '):
+
+            return None
+
+        return authorization_header.split(' ')[1]
