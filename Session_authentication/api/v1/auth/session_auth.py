b0bf20bc6e5fffbb1c152859f60276593043f712
BradenE19 <6115@holbertonstudents.com>
2024-01-22 15:22:24 -0600

    task 8 try

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -28,7 +28,23 @@ class SessionAuth(Auth):
         return None
 
     def current_user(self, request=None):
-        '''self descriptive'''
+        """current user id"""
         session_cookie = self.session_cookie(request)
         session_id = self.user_id_for_session_id(session_cookie)
         return User.get(session_id)
+
+    def destroy_session(self, request=None):
+        """session teardown"""
+        if not request:
+            return False
+        session_cookie = self.session_cookie(request)
+
+        if not session_cookie:
+            return False
+
+        user_id = self.user_id_for_session_id(session_cookie)
+
+        if not user_id:
+            return False
+        self.user_id_by_session_id.pop(session_cookie)
+        return True
