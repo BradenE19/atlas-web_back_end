57055f1af48a7d31a3e4a5bde27b87c5b6e4863c
BradenE19 <6115@holbertonstudents.com>
2024-01-23 13:05:33 -0600

    trying to fix db.py task1

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-"""function returns a <class 'user.User'>
-parameter email is annotated as a <class 'str'>
-parameter hashed_password is annotated as a <class 'str'>"""
+''' Module data base '''
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -14,9 +12,7 @@ from sqlalchemy.orm.exc import NoResultFound
 class DB:
 
     def __init__(self):
-        """function returns a <class 'user.User'>
-parameter email is annotated as a <class 'str'>
-parameter hashed_password is annotated as a <class 'str'>"""
+        ''' def init '''
         self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
@@ -24,31 +20,15 @@ parameter hashed_password is annotated as a <class 'str'>"""
 
     @property
     def _session(self):
-        """function returns a <class 'user.User'>
-parameter email is annotated as a <class 'str'>
-parameter hashed_password is annotated as a <class 'str'>"""
+        ''' def session '''
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
-        """function returns a <class 'user.User'>
-parameter email is annotated as a <class 'str'>
-parameter hashed_password is annotated as a <class 'str'>"""
+        ''' def add user '''
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
-        return user
-
-    def find_user_by(self, **kargs) -> TypeVar('User'):
-        """function returns a <class 'user.User'>
-parameter email is annotated as a <class 'str'>
-parameter hashed_password is annotated as a <class 'str'>"""
-        try:
-            user = self._session.query(User).filter_by(**kargs).first()
-        except TypeError:
-            raise InvalidRequestError
-        if user is None:
-            raise NoResultFound
         return user
