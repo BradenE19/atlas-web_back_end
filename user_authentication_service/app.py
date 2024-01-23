022150723795ca0f4f33e03cd9a4651114605a5e
BradenE19 <6115@holbertonstudents.com>
2024-01-23 14:02:10 -0600

    task 7 app.py

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -17,5 +17,23 @@ def message_Bienvenue() -> str:
     return jsonify(msg)
 
 
+@app.route('/users', methods=['POST'])
+def users() -> str:
+
+    try:
+        email = request.form['email']
+        password = request.form['password']
+    except KeyError:
+        abort(400)
+
+    try:
+        user = AUTH.users(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+    
+    msg = {"email": email, "message": "user created"}
+    return jsonify(msg)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
