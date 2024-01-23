c740de68662caaae9b9bd6f5b615416650529a6d
BradenE19 <6115@holbertonstudents.com>
2024-01-23 13:45:24 -0600

    task 5

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -12,3 +12,25 @@ from typing import Union
 def _hash_password(password: str) -> str:
     """ Returns a salted hash of the input password """
     return hashpw(password.encode('utf-8'), gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Takes mandatory email and password,
+        returns new user object"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            user = self._db.add_user(email, hashed_password)
+
+            return user
+
+        else:
+            raise ValueError(f'User {email} already exists')
