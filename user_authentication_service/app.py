4f9ebeba92f6f39603140be1c58c63461e9d0bf4
BradenE19 <6115@holbertonstudents.com>
2024-01-23 14:05:00 -0600

    app.py fix?

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -12,13 +12,13 @@ AUTH = Auth()
 
 
 @app.route('/', methods=['GET'])
-def message_Bienvenue() -> str:
+def hello_world() -> str:
     msg = {"message": "Bienvenue"}
     return jsonify(msg)
 
 
 @app.route('/users', methods=['POST'])
-def users() -> str:
+def register_user() -> str:
 
     try:
         email = request.form['email']
@@ -27,13 +27,9 @@ def users() -> str:
         abort(400)
 
     try:
-        user = AUTH.users(email, password)
+        user = AUTH.register_user(email, password)
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
-    
+
     msg = {"email": email, "message": "user created"}
     return jsonify(msg)
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
