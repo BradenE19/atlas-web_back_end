dd7725325d2408f69626af41e1901b8a38bd739d
BradenE19 <6115@holbertonstudents.com>
2024-01-23 13:21:19 -0600

    task 1 hoping fix

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python3
-''' Module data base '''
+"""DB module
+"""
+import logging
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from user import Base, User
-from typing import TypeVar
-from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.session import Session
 from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+from user import Base, User
+
+# Set SQLAlchemy log level: only show error messages and above
+logging.basicConfig()
+logging.getLogger('sqlalchemy.engine').setLevel(logging.ERROR)
 
 
 class DB:
+    """DB class
+    """
 
     def __init__(self):
-        ''' def init '''
+        """Initialize a new DB
+        """
         self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
@@ -20,15 +29,21 @@ class DB:
 
     @property
     def _session(self):
-        ''' def session '''
+        """Memoized session object"""
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
-        ''' def add user '''
+        """Method saves user to DB,
+        adds new user to new session,
+        saves user to DB"""
+        # Create user object
         user = User(email=email, hashed_password=hashed_password)
+        # Add user to session
         self._session.add(user)
+        # Save user to Db
         self._session.commit()
+        # Return new user object
         return user
