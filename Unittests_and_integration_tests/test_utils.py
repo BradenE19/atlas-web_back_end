e0f627c62d5f0d378bdc2597493e713cfa2c8c33
BradenE19 <6115@holbertonstudents.com>
2024-01-26 14:31:41 -0600

    retry task 0

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -4,33 +4,14 @@ import unittest
 from utils import access_nested_map
 
 class TestAccessNestedMap(unittest.TestCase):
-    def test_access_nested_map(self):
-        nested_map = {
-            "a": {
-                "b": {
-                    "c": 1
-                }
-            }
-        }
-
-        result1 = access_nested_map(nested_map, ["a", "b", "c"])
-        self.assertEqual(result1, 1)
-
-        with self.assertRaises(KeyError):
-            access_nested_map(nested_map, ["a", "x"])
-
-        
-        with self.assertRaises(KeyError):
-            access_nested_map(nested_map, ["a", "b", "x"])
-
-
-        with self.assertRaises(KeyError):
-            access_nested_map(nested_map, ["x"])
-
-
-        result5 = access_nested_map(nested_map, [])
-        self.assertEqual(result5, nested_map)
-
+    """Unit Test for access_nested_map"""
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected_result):
+        self.assertEqual(access_nested_map(nested_map, path), expected_result)
 
 if __name__ == '__main__':
     unittest.main()
