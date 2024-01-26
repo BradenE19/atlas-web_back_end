05ef259a0d5ea278595dfb8d57b34b14bf444059
BradenE19 <6115@holbertonstudents.com>
2024-01-26 15:06:06 -0600

    retry task 1

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -13,12 +13,16 @@ class TestAccessNestedMap(unittest.TestCase):
     ])
     def test_access_nested_map(self, nested_map, path, expected_result):
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
-    
-    def test_access_nested_map_exception(self, map, path, wrong_output):
+
+    @parameterized.expand([
+        ({}, ("a",), 'a'),
+        ({"a": 1}, ("a", "b"), 'b')
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected_result):
         """ test that method raises correct exception """
-        with self.assertRaises(KeyError) as e:
-            access_nested_map(map, path)
-            self.assertEqual(wrong_output, e.exception)
+        with self.assertRaises(KeyError) as raises:
+            access_nested_map(nested_map, path)
+            self.assertEqual(expected_result, raises.exception)
 
 
 if __name__ == '__main__':
