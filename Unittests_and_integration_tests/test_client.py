975705c18ba107d8b1404ad6fcad210f49426efe
BradenE19 <6115@holbertonstudents.com>
2024-01-29 13:36:28 -0600

    task5 try

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """unit tests for utils"""
 import unittest
+import fixtures
+from fixtures import TEST_PAYLOAD
 from unittest import TestCase, mock
-from unittest.mock import patch, Mock
+from unittest.mock import patch, Mock, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -21,6 +23,27 @@ class TestGithubOrgClient(unittest.TestCase):
 
         mock_get_json.assert_called_once_with(
             f'https://api.github.com/orgs/{org_name}')
+        
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get_json):
+        """test that public repos returns expected result"""
+        mock_repos_payload = [{'name': 'repo1'}, {'name': 'repo2'}]
+        mock_get_json.return_value = mock_repos_payload
+        mock_public_repos_url = 'http://mocked.url/repos'
+
+        with patch.object(GithubOrgClient,
+                          'public_repos_url',
+                          new_callable=PropertyMock) as mock_repo_url:
+            mock_repo_url,return_value = mock_public_repos_url
+
+            github_org_client = GithubOrgClient("some_org")
+
+            public_repos = github_org_client.public_repos()
+
+            self.assertEqual(public_repos, [repo['name'] for repo in mock_repos_payload])
+
+            mock_repo_url.assert_called_once()
+            mock_get_json.assert_called_once_with(mock_public_repos_url)
 
 
 if __name__ == '__main__':
