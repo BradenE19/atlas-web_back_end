90d2c39671f9a8b8cdd2c70009792d606364a730
BradenE19 <6115@holbertonstudents.com>
2024-01-31 11:58:53 -0600

    task4 attempt

---
+++ i18n/4-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Module for i18n"""
+from flask_babel import Babel
+from flask import Flask, render_template, request, locale
+
+app = Flask(__name__, template_folder='templates')
+babel = Babel(app)
+
+
+class Config(object):
+    """configuration class"""
+
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def hello_world() -> str:
+    """Renders a Basic Template for Babel Implementation"""
+    return render_template("1-index.html")
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """determines best match with supported translated languages"""
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == "__main__":
+    app.run()
