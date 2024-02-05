140f734395e8e34a0e27dcec5228ee309c5a092c
BradenE19 <6115@holbertonstudents.com>
2024-02-05 12:13:00 -0600

    task1 try

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Writes strings to Redis"""
 import redis
-from typing import Union
+from typing import Union, Optional, Callable
 from uuid import uuid4, UUID
 
 class Cache():
@@ -12,10 +12,33 @@ class Cache():
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """Store the input data in Redis using a
-        random key and return the key.
+        """store input daata in Redis
+        use random key to return key
         """
         random_key = str(uuid4())
         self._redis.set(random_key, data)
 
         return random_key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """convert data to desired format"""
+        value = self._redis.get(key)
+        if fn:
+            value = fn(value)
+
+        return value
+
+    def get_str(self, key: str) -> str:
+        """ Parameterizes a value from redis to str """
+        value = self._redis.get(key)
+        return value.decode("utf-8")
+
+    def get_int(self, key: str) -> int:
+        """ Parameterizes a value from redis to int """
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode("utf-8"))
+        except Exception:
+            value = 0
+        return value
