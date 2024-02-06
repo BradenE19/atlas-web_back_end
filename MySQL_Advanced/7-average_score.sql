35aa0d4bbd3db705720ce1588d655844ecbefaa8
BradenE19 <6115@holbertonstudents.com>
2024-02-06 14:40:22 -0600

    task7 fix

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -1,29 +1,13 @@
 -- creates a stored procedure that computes and store the average score for a student
 -- Create the stored procedure
-DELIMITER //
-
-CREATE PROCEDURE ComputeAverageScoreForUser (
-    IN user_id INT
-)
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageScoreForUser(
+    IN user_id INT)
 BEGIN
-    DECLARE avg_score DECIMAL(10, 2);
-
-    -- Calculate the average score for the user
-    SELECT AVG(score) INTO avg_score
-    FROM corrections
-    WHERE user_id = user_id;
-
-    -- Update the average score for the user
-    IF EXISTS (SELECT 1 FROM average_scores WHERE user_id = user_id) THEN
-        UPDATE average_scores
-        SET average_score = avg_score
-        WHERE user_id = user_id;
-    ELSE
-        INSERT INTO average_scores (user_id, average_score)
-        VALUES (user_id, avg_score);
-    END IF;
-    
-    
-END //
-
+    DECLARE avg_score FLOAT;
+    SET avg_score = (SELECT AVG(score) FROM corrections AS C WHERE C.user_id=user_id);
+    UPDATE users SET average_score = avg_score WHERE id=user_id;
+END
+$$
 DELIMITER ;
