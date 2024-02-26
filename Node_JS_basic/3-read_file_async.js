136467f839321ec55d3577eed8b4ea07b517473d
BradenE19 <6115@holbertonstudents.com>
2024-02-26 13:18:58 -0600

    task3 fix

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -4,28 +4,45 @@ function countStudents(path) {
   return new Promise((resolve, reject) => {
     fs.readFile(path, 'utf8', (err, data) => {
       if (err) {
-        reject(new Error('Cannot load the database'));
-      } else {
-        const students = data.trim().split('\n').slice(1).map(line => line.split(','));
-        const fields = {};
-        students.forEach(student => {
-          for (let i = 0; i < student.length - 1; i++) {
-            if (!fields[student[i]]) {
-              fields[student[i]] = [];
-            }
-            fields[student[i]].push(student[0]);
-          }
-        });
-
-        const totalCount = students.length;
-        console.log(`Number of students: ${totalCount}`);
-
-        Object.entries(fields).forEach(([field, students]) => {
-          console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
-        });
-
-        resolve(totalCount);
+        reject(Error('Cannot load the database'));
+        return;
       }
+      const response = [];
+      let msg;
+
+      const content = data.toString().split('\n');
+
+      let students = content.filter((item) => item);
+
+      students = students.map((item) => item.split(','));
+
+      const NUMBER_OF_STUDENTS = students.length ? students.length - 1 : 0;
+      msg = `Number of students: ${NUMBER_OF_STUDENTS}`;
+      console.log(msg);
+
+      response.push(msg);
+
+      const fields = {};
+      for (const i in students) {
+        if (i !== 0) {
+          if (!fields[students[i][3]]) fields[students[i][3]] = [];
+
+          fields[students[i][3]].push(students[i][0]);
+        }
+      }
+
+      delete fields.field;
+
+      for (const key of Object.keys(fields)) {
+        msg = `Number of students in ${key}: ${
+          fields[key].length
+        }. List: ${fields[key].join(', ')}`;
+
+        console.log(msg);
+
+        response.push(msg);
+      }
+      resolve(response);
     });
   });
 }
